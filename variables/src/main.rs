b90b027c121a161a489dac97f5ff1021614916ab
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-06-18 11:17:30 +0200

    fn

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    const MAX_NUMBER: u32 = 1000;
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    increase_num(&mut x);
+    println!("The value of x is: {}", x);
+
+    let y = 5;
+    println!("The value of y is: {}", y);
+    let y = "six";
+    println!("The value of y is: {}", y);
+}
+
+fn increase_num(num: &mut i32) {
+    *num += 1;
 }
