dfdf0cf8e08fd966c770cb2bcc93c70cff6cc9b0
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-06-18 16:44:53 +0200

    more ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -22,4 +22,45 @@ fn main() {
     let s1 = String::from("hello");
     let s2 = s1.clone(); // Copy / clone
     println!("{} {} world", s1, s2); // can still use s1 here
+
+    let s = String::from("hello");
+    takes_ownership(s);
+    // println!("s {}", s); // can't do that because ownership has been taken
+
+    let x = 33;
+    makes_copy(x);
+    println!("I still can print out {}", x);
+
+    let s = gives_ownership();
+    println!("{}", s);
+
+    let s3 = takes_and_gives_back(s);
+    println!("{}", s3);
+
+    let (s3, len) = calculate_length(s3);
+    println!("{} has length of: {}", s3, len);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("I'm the owner now of {}", some_string)
+}
+
+// integers are copied
+fn makes_copy(some_integer: i32) {
+    println!("I copied {}", some_integer);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("You are the owner now!");
+
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
