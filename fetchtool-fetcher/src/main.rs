ea0256fe9f0587059bcbb1b071f2e456cc6a7f44
Sascha Loishandl <sascha@Saschas-MacBook-Air.local>
2026-05-17 10:27:25 +0200

    counts

--- fetchtool-fetcher/src/main.rs
+++ fetchtool-fetcher/src/main.rs
@@ -3,12 +3,15 @@ mod commands;
 const CMDS: &[&str] = &["fastfetch", "neofetch", "gofetch", "hyfetch", "cpufetch"];
 
 fn main() {
+    let mut existing: usize = 0;
+    let all_count = CMDS.len();
     for s in CMDS {
         if commands::exists(s) {
             println!("{s}");
-        }
-        else {
+            existing = existing + 1;
+        } else {
             println!("!{s}");
         }
     }
+    println!("Existing: {existing} out of {all_count}");
 }
